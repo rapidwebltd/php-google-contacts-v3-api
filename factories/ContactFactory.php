8d80b7e217fb6b8228e5ed74005e5a8197d66003
Jordan Hall <jordan.hall@rapidweb.biz>
2015-07-27 13:07:58 +0100

    Ability to get a single Contact object from a Google provide Contact entry self URL

--- factories/ContactFactory.php
+++ factories/ContactFactory.php
@@ -55,7 +55,54 @@ abstract class ContactFactory
 
         return $contactsArray;
     }
+    
+    public static function getBySelfURL($selfURL)
+    {
+        $client = GoogleHelper::getClient();
 
+        $req = new \Google_Http_Request($selfURL);
+
+        $val = $client->getAuth()->authenticatedRequest($req);
+
+        $response = $val->getResponseBody();
+        
+        $xmlContact = simplexml_load_string($response);
+        $xmlContact->registerXPathNamespace('gd', 'http://schemas.google.com/g/2005');
+        
+        $xmlContactsEntry = $xmlContact;
+        
+        $contactDetails = array();
+
+        $contactDetails['id'] = (string) $xmlContactsEntry->id;
+        $contactDetails['name'] = (string) $xmlContactsEntry->title;
+
+        foreach ($xmlContactsEntry->children() as $key => $value) {
+            $attributes = $value->attributes();
+            
+            if ($key == 'link') {
+                if ($attributes['rel'] == 'edit') {
+                    $contactDetails['editURL'] = (string) $attributes['href'];
+                } elseif ($attributes['rel'] == 'self') {
+                    $contactDetails['selfURL'] = (string) $attributes['href'];
+                }
+            }
+        }
+
+        $contactGDNodes = $xmlContactsEntry->children('http://schemas.google.com/g/2005');
+
+        foreach ($contactGDNodes as $key => $value) {
+            $attributes = $value->attributes();
+
+            if ($key == 'email') {
+                $contactDetails[$key] = (string) $attributes['address'];
+            } else {
+                $contactDetails[$key] = (string) $value;
+            }
+        }
+
+        return new Contact($contactDetails);
+    }
+    
     public static function update(Contact $contact)
     {
         
