ed3c8324b54902256343e3ac47aeb9ccaa752c2d
Jordan Hall <jordan.hall@rapidweb.biz>
2015-07-27 15:31:38 +0100

    Added page to test individual contact retrieval and updating

---
+++ test_individual.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use rapidweb\googlecontacts\factories\ContactFactory;
+
+if (!isset($_GET['selfURL'])) {
+    throw new Exception('No selfURL specified.');
+}
+
+$contact = ContactFactory::getBySelfURL($_GET['selfURL']);
+
+var_dump($contact);
+
+$contact->name = 'Test';
+$contact->phoneNumber = '07812363789';
+$contact->email = 'test@example.com';
+
+$contactAfterUpdate = ContactFactory::submitUpdates($contact);
+
+var_dump($contactAfterUpdate);
