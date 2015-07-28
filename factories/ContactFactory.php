69dc6b9a0bff7e38e3ab6345354ff3a5893b739f
Jordan Hall <jordan.hall@rapidweb.biz>
2015-07-28 11:42:43 +0100

    Create ContactFactory create contact function

--- factories/ContactFactory.php
+++ factories/ContactFactory.php
@@ -180,4 +180,80 @@ abstract class ContactFactory
 
         return new Contact($contactDetails);
     }
+    
+    public static function create($name, $phoneNumber, $emailAddress)
+    {
+        // create new entry
+		$doc  = new \DOMDocument();
+		$doc->formatOutput = true;
+		$entry = $doc->createElement('atom:entry');
+		$entry->setAttributeNS('http://www.w3.org/2000/xmlns/' , 'xmlns:atom', 'http://www.w3.org/2005/Atom');
+		$entry->setAttributeNS('http://www.w3.org/2000/xmlns/' , 'xmlns:gd', 'http://schemas.google.com/g/2005');
+		$entry->setAttributeNS('http://www.w3.org/2000/xmlns/' , 'xmlns:gd', 'http://schemas.google.com/g/2005');
+		$doc->appendChild($entry);
+							
+		// add name element
+		$title = $doc->createElement('title', $name);
+		$entry->appendChild($title);
+		
+		// add email elements
+	    $email = $doc->createElement('gd:email'); 
+	    $email->setAttribute('rel' ,'http://schemas.google.com/g/2005#work');
+	    $email->setAttribute('address', $emailAddress);
+	    $entry->appendChild($email);
+							
+		// add phone number element
+		$contact = $doc->createElement('gd:phoneNumber', $phoneNumber);
+		$contact->setAttribute('rel' ,'http://schemas.google.com/g/2005#work');
+		$entry->appendChild($contact);
+		
+		$xmlToSend = $doc->saveXML();
+		
+		$client = GoogleHelper::getClient();
+		
+		$req = new \Google_Http_Request('https://www.google.com/m8/feeds/contacts/default/full');
+        $req->setRequestHeaders(array('content-type' => 'application/atom+xml; charset=UTF-8; type=feed'));
+        $req->setRequestMethod('POST');
+        $req->setPostBody($xmlToSend);
+
+        $val = $client->getAuth()->authenticatedRequest($req);
+
+        $response = $val->getResponseBody();
+
+        $xmlContact = simplexml_load_string($response);
+        $xmlContact->registerXPathNamespace('gd', 'http://schemas.google.com/g/2005');
+
+        $xmlContactsEntry = $xmlContact;
+
+        $contactDetails = array();
+
+        $contactDetails['id'] = (string) $xmlContactsEntry->id;
+        $contactDetails['name'] = (string) $xmlContactsEntry->title;
+
+        foreach ($xmlContactsEntry->children() as $key => $value) {
+            $attributes = $value->attributes();
+
+            if ($key == 'link') {
+                if ($attributes['rel'] == 'edit') {
+                    $contactDetails['editURL'] = (string) $attributes['href'];
+                } elseif ($attributes['rel'] == 'self') {
+                    $contactDetails['selfURL'] = (string) $attributes['href'];
+                }
+            }
+        }
+
+        $contactGDNodes = $xmlContactsEntry->children('http://schemas.google.com/g/2005');
+
+        foreach ($contactGDNodes as $key => $value) {
+            $attributes = $value->attributes();
+
+            if ($key == 'email') {
+                $contactDetails[$key] = (string) $attributes['address'];
+            } else {
+                $contactDetails[$key] = (string) $value;
+            }
+        }
+        
+        return new Contact($contactDetails);
+    }
 }
